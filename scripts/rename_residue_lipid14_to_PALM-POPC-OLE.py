e12e1bd6e02d6855c0dd019a196a52687a4f2179
Josef Melcr <melcr@uochb.cas.cz>
2018-01-29 11:03:24 +0100

    Update to a very specific scritpt that simply renamed parts of POPC residue to Palm POPC Ole -- not working with recent MDAnalysis library:          The update to MDAnalysis has lead to          an unfortunate (to me and this script)          lack of special function of this scipt          i.e. it does not give a special resname to          subsections of a single residue.          As I plant to work with IUPAB (Charmm36) style of atom naming in the future,          I'm not correcting this in this script and leave it malfunctioning.

--- scripts/rename_residue_lipid14_to_PALM-POPC-OLE.py
+++ scripts/rename_residue_lipid14_to_PALM-POPC-OLE.py
@@ -11,6 +11,17 @@
 # the head group segment beginning with O12 and ending with O22
 # the rest (begin/end) being the tails PALM/OLE.
 
+
+# last edit 29-Jan 2018 by Josef Melcr
+
+# BEWARE: The update to MDAnalysis has lead to 
+#         an unfortunate (to me and this script) 
+#         lack of special function of this scipt
+#         i.e. it does not give a special resname to 
+#         subsections of a single residue.
+#         As I plant to work with IUPAB (Charmm36) style of atom naming in the future,
+#         I'm not correcting this in this script and leave it malfunctioning. 
+
 import MDAnalysis as mda
 from optparse import OptionParser
 
@@ -21,32 +32,42 @@ parser.add_option('-i', '--inp',  dest='inp_fname',  help='input MDanalysis topo
 parser.add_option('-o', '--out',  dest='out_fname',  help='output MDanalysis topology file name', default="last_frame_nonwat.gro")
 opts, args = parser.parse_args()
 
-# conf_init.gro should always be present in the sim dir
+print """
+ BEWARE: The update to MDAnalysis has lead to 
+         an unfortunate (to me and this script) 
+         lack of special function of this scipt
+         i.e. it does not give a special resname to 
+         subsections of a single residue.
+         As I plant to work with IUPAB (Charmm36) style of atom naming in the future,
+         I'm not correcting this in this script and leave it malfunctioning. 
+"""
+
 u = mda.Universe(opts.inp_fname)
 
 # find some POPC residue
-some_popc_res = None
 try:
     for r in u.residues:
-        if r.name == "POPC":
-            some_popc_res = r
+        if r.resname == "POPC":
+            # find the indices of the first/last head group atoms
+            for a in r.atoms:
+                if a.name == "O12":
+                    headgr_begin_index = a.id
+                if a.name == "O22":
+                    headgr_end_index = a.id
             break
-except:
-    print "Couldn't find a POPC residue -- already renamed to PALM-POPC-OLE?"
 
-if some_popc_res != None:
-    # find the indices of the first/last head group atoms
-    headgr_begin_index = some_popc_res.names.tolist().index("O12")
-    headgr_end_index   = some_popc_res.names.tolist().index("O22")
 
     # resname the resnames of PALM and OLE segments
     for r in u.residues:
-        if r.name == "POPC":
+        if r.resname == "POPC":
             for a in r.atoms[:headgr_begin_index]:
-                a.resname = "PALM"
+                a.residue.resname = "PALM"
             for a in r.atoms[headgr_end_index+1:]:
-                a.resname = "OLE"
+                a.residue.resname = "OLE"
 
     # write the new topology out
-    u.SYSTEM.write(opts.out_fname)
+    u.trajectory.Writer(opts.out_fname)
+
+except:
+    print "Couldn't find a POPC residue -- already renamed to PALM-POPC-OLE?"
 
